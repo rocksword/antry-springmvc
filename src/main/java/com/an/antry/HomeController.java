e657be1381d3d5be4809185bcc74ddbba5f11bb3
rocksword <rocksword@163.com>
2014-12-19 16:23:18 +0800

    Init project

---
+++ src/main/java/com/an/antry/HomeController.java
@@ -0,0 +1,41 @@
+package com.an.antry;
+
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+
+    /**
+     * when access root url "/", show home page
+     * 
+     * @param locale
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String root(Locale locale, Model model) {
+        LOGGER.info("root");
+        return "home";
+    }
+
+    /**
+     * when access home url "/home", show home page
+     * 
+     * @param locale
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    public String home(Locale locale, Model model) {
+        LOGGER.info("home");
+        return "home";
+    }
+}
